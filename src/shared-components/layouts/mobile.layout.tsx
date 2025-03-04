2f5237fa0a49f39b60519c8be2308759b893a7bd
krist.dev.vn <krist.dev.vn@gmail.com>
2025-03-04 16:11:42 +0700

    Add scroll reset on mobile layout for route changes

--- src/shared-components/layouts/mobile.layout.tsx
+++ src/shared-components/layouts/mobile.layout.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
 export default function MobileLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
+  useEffect(() => {
+    window.scrollTo(1, 0);
+  }, [pathName]);
+
   return (
     <main className="container max-w-md overflow-hidden mx-auto bg-background min-h-screen h-screen">
       {children}
